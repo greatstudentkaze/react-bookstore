9efd25470d8551e5f70df88d27a21a18b6c2109a
gsk <dfyz0r@list.ru>
2021-01-29 20:21:28 +0300

    feat: add base styles, normalize.css

--- src/components/styled/GlobalStyle.js
+++ src/components/styled/GlobalStyle.js
@@ -1,7 +1,43 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
+import { normalize } from './normalize';
+import { colors } from './variables';
 
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+  
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+  
+  a {
+    text-decoration: none;
+  }
+  
+  html, 
+  body {
+    box-sizing: border-box;
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    
+    font-family: "Tahoma", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    
+    background-color: ${colors.white};
+    
+    cursor: default;
+  }
 `;
 
 export default GlobalStyle;
