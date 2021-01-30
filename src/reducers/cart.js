588492133a17d2105ccfa4be46a2ccefb9fc64bd
gsk <dfyz0r@list.ru>
2021-01-30 16:08:33 +0300

    feat: change cart reducer logic and add cart action types

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,5 +1,21 @@
-export const cart = (state = {}, action) => {
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../types/cart';
+
+const initialState = {
+  items: [],
+};
+
+export const cart = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      }
+    case REMOVE_FROM_CART:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload),
+      }
     default:
       return state;
   }
---
+++ src/types/cart.js
@@ -0,0 +1,2 @@
+export const ADD_TO_CART = 'CART:ADD';
+export const REMOVE_FROM_CART = 'CART:REMOVE';
