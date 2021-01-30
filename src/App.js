4d1498d86fa790e9a20e12f47334ae02f6de258f
gsk <dfyz0r@list.ru>
2021-01-30 21:42:19 +0300

    add todo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import Catalog from './components/Catalog';
 
 const App = () => {
+  // todo: Добавить фильтрацию по популярности (в массиве products у объектов есть свойство rating)
 
   return (
     <>
